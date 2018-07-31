4d5a480adb7c2ac6c73f37c9fa0e6ac5287ad7d8
SubasishMohapatra <subasish.mohapatra@daimler.com>
2018-07-31 09:56:58 +0530

    Added Infrastructure project as part of solution

---
+++ Infrastructure/ResourceManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Globalization;
+
+namespace Infrastructure
+{
+    /// <summary>
+    /// Provides convenient access to culture-specific resources at run time
+    /// </summary>
+    public class ResourceManager : System.Resources.ResourceManager
+    {
+        public ResourceManager()
+        { }
+
+        public ResourceManager(CultureInfo cultureInfo)
+        {
+            this.CultureInfo = cultureInfo;
+        }
+
+        public CultureInfo CultureInfo { get; set; }
+    }
+}
---
+++ Infrastructure/Translation.cs
@@ -0,0 +1,23 @@
+﻿using CommonServiceLocator;
+using Infrastructure.Properties;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class Translation
+    {
+        //static ResourceManager resourceManager = null;
+        public static string GetTranslatedText(string key)
+        {
+            var resourceManager = ServiceLocator.Current.GetInstance<ResourceManager>();
+            var translatedText= Resources.ResourceManager.GetString(key, new CultureInfo(resourceManager.CultureInfo.Name));
+            return translatedText;
+        }
+    }
+}
